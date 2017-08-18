032b215b590268fb00e7cf352892d3fe8493b69d
zhangjie <1217336430@qq.com>
2017-08-18 18:02:23 +0800

    xinzeng tiaohzhuan

--- aboutUs/js/index.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  
-     $('.qq').click(function() {
-        //  location.href = '//shang.qq.com/wpa/qunwpa?idkey=222ac03f03170f6610d3171c8476231c575897c7ec2d071b1a9846f8b10b8354'
-        // location.href = 'http://shang.qq.com/wpa/qunwpa?idkey=QgVho5c0xKlX7hsOQCTglO2JKqeSosWY'
-        // location.href = 'mqqapi://card/show_pslcard?src_type=internal&version=1&uin=180275927@&key=QgVho5c0xKlX7hsOQCTglO2JKqeSosWY@&card_type=group&source=externa'
-    })
-})
